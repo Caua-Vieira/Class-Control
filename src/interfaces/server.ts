9969b811b195c60bc2aef08941911c3c96e3aa5e
Caua-Vieira <cauasilva8@gmail.com>
2025-09-03 22:35:36 -0300

    feat: criar conexão com banco de dados

---
+++ src/interfaces/server.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import { Container } from "typescript-ioc";
+import YAML from 'yamljs';
+
+const app = express();
+app.use(express.json());
+
+// app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// app.use(errorHandler);
+
+export default app;
