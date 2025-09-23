c745dc7ec314eed102815e83b44f3183f9c4d052
Caua-Vieira <cauasilva8@gmail.com>
2025-09-22 23:36:09 -0300

    chore: remover códigos comentados

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,6 @@ import { Database } from './infrastructure/database/database';
 
 const PORT = process.env.PORT || 3000;
 
-// AppDataSource.initialize()
-//     .then(() => {
-//         console.log("Banco de dados conectado!");
-//         app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-//     })
-//     .catch((err) => console.error("Erro ao conectar no banco: ", err));
-
 async function startServer() {
     try {
         const database = Container.get(Database);
