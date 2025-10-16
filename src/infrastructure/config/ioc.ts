537d611e30f0fac7eac61e8a58e7b26df3455f37
Caua-Vieira <cauasilva8@gmail.com>
2025-10-15 21:55:27 -0300

    fix: bind do fluxo de publicação na fila

--- src/infrastructure/config/ioc.ts
+++ src/infrastructure/config/ioc.ts
@@ -3,6 +3,9 @@ import { LoginRepository } from "../../domain/contracts/login-repository";
 import { HttpLoginRepository } from "../repositories/auth-repository/http-login-repository";
 import { TasksRepository } from "../../domain/contracts/tasks-repository";
 import { HttpTasksRepository } from "../repositories/tasks-repository/http-tasks-repository";
+import { MessageQueue } from "../../domain/contracts/messaging/message-queue";
+import { RabbitMQService } from "../messaging/rabbitmq-service";
 
 Container.bind(LoginRepository).to(HttpLoginRepository);
 Container.bind(TasksRepository).to(HttpTasksRepository);
+Container.bind(MessageQueue).to(RabbitMQService);
