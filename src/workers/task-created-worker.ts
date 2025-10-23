a6456bb670e713f1398f2fd1c75a37a842d5d2dd
Caua-Vieira <cauasilva8@gmail.com>
2025-10-22 23:26:49 -0300

    feat: implementar envio de email

--- src/workers/task-created-worker.ts
+++ src/workers/task-created-worker.ts
@@ -1,4 +1,6 @@
 import amqp from "amqplib";
+import { EmailService } from "../domain/contracts/email/email-service";
+import { Container } from "typescript-ioc";
 
 async function startWorker() {
     try {
@@ -8,6 +10,8 @@ async function startWorker() {
         const queue = "task_created";
         await channel.assertQueue(queue, { durable: true });
 
+        const emailService = Container.get(EmailService);
+
         console.log(`Aguardando mensagens na fila: ${queue}`);
 
         channel.consume(queue, async (msg) => {
@@ -16,7 +20,20 @@ async function startWorker() {
 
                 console.log("Mensagem recebida:", content);
 
-                channel.ack(msg);
+                try {
+                    const content = JSON.parse(msg.content.toString());
+                    const { title, description, userEmail } = content;
+
+                    await emailService.sendEmail(
+                        userEmail,
+                        "Nova tarefa criada",
+                        `<h3>${title}</h3><p>${description}</p>`
+                    );
+
+                    channel.ack(msg);
+                } catch (error) {
+                    console.log("Erro ao processar mensagem:", error);
+                }
             }
         });
     } catch (error) {
