e5e5b8de779f85c61b28487d8ba9e75a115bc2c6
Caua-Vieira <cauasilva8@gmail.com>
2025-11-12 23:18:08 -0300

    fix: corrigir conexão com banco de dados no teste do cronjob

--- src/task-reminder-cron.ts
+++ src/task-reminder-cron.ts
@@ -1,10 +1,23 @@
 import cron from "node-cron";
 import { checkUpcomingTasks } from "./workers/task-reminder-worker";
 import './infrastructure/config/ioc';
+import { Container } from "typescript-ioc";
+import { Database } from "./infrastructure/database/database";
 
-cron.schedule("*/10 * * * * *", async () => {
-    console.log("⏰ Executando verificação de tarefas...");
-    await checkUpcomingTasks();
-});
+async function startCron() {
+    const database = Container.get(Database);
+    await database.connect();
 
-console.log("✅ Cronjob iniciado.");
+    console.log("✅ Cronjob iniciado.");
+
+    cron.schedule("*/10 * * * * *", async () => {
+        try {
+            console.log("⏰ Executando verificação de tarefas...");
+            await checkUpcomingTasks();
+        } catch (error) {
+            console.error("[NODE-CRON] [ERROR]", error);
+        }
+    });
+}
+
+startCron();
