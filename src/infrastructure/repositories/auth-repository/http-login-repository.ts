da7e2cc8f001b26170a1c2fe5949ecca8195c773
Caua-Vieira <cauasilva8@gmail.com>
2026-03-31 22:52:39 -0300

    feat: adicionar logs no login

--- src/infrastructure/repositories/auth-repository/http-login-repository.ts
+++ src/infrastructure/repositories/auth-repository/http-login-repository.ts
@@ -3,14 +3,22 @@ import { LoginRepository } from "../../../domain/contracts/login-repository";
 import { User } from "../../../domain/entities/user";
 import { Database } from "../../database/database";
 import { DatabaseException } from "../../../domain/errors/errors";
+import { logger } from "../../config/logger";
 
 export class HttpLoginRepository implements LoginRepository {
     constructor(@Inject private database: Database) { }
 
     async findLoginByEmail(email: string): Promise<User | null> {
         try {
-            return this.database.appDataSource.getRepository(User).findOne({ where: { email } });
+            const user = await this.database.appDataSource.getRepository(User).findOne({ where: { email } });
+
+            if (!user) {
+                logger.warn({ email }, 'user not found on database');
+            }
+
+            return user;
         } catch (error) {
+            logger.error({ email, error: (error as Error).message }, 'database error on findLoginByEmail');
             throw new DatabaseException("Ocorreu um erro ao buscar informações");
         }
     }
