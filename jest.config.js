07550b392854767425b63fcad4a5bd3d80f63362
Caua-Vieira <cauasilva8@gmail.com>
2026-05-11 22:32:18 -0300

    config: configurar jest para testes

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,27 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
   testEnvironment: "node",
+  testMatch: ["**/tests/**/*.test.ts"],
+  setupFiles: ["reflect-metadata"],
   transform: {
-    "^.+\.tsx?$": ["ts-jest",{}],
+    "^.+\\.tsx?$": ["ts-jest", {
+      tsconfig: "tsconfig.test.json",
+    }],
   },
+  collectCoverageFrom: [
+    "src/**/*.ts",
+    "!src/index.ts",
+    "!src/task-reminder-cron.ts",
+    "!src/infrastructure/config/ioc.ts",
+    "!src/infrastructure/config/logger.ts",
+    "!src/infrastructure/database/**",
+    "!src/infrastructure/messaging/**",
+    "!src/infrastructure/email/nodemailer-service.ts",
+    "!src/infrastructure/repositories/**",
+    "!src/interfaces/server.ts",
+    "!src/interfaces/routes/**",
+    "!src/workers/task-created-worker.ts",
+  ],
+  coverageReporters: ["text", "lcov"],
+  coverageDirectory: "coverage",
 };
